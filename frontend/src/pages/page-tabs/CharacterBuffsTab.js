8189d92c4ca08a344ff487010e75754b219f6de4
Tristan <127442375+tyoung-99@users.noreply.github.com>
2024-02-09 17:26:11 -0500

    Added buffs page

---
+++ frontend/src/pages/page-tabs/CharacterBuffsTab.js
@@ -0,0 +1,42 @@
+// Character's current buffs/debuffs
+
+const CharacterBuffsTab = ({ character }) => {
+  const parseBuff = (currentBuff, i) => (
+    <div key={i}>
+      <h2 className="float-right">
+        {currentBuff.spell ||
+          currentBuff.monster ||
+          currentBuff.condition ||
+          currentBuff.ability}
+      </h2>
+      <h1>{currentBuff.name}</h1>
+      {currentBuff.desc.map((paragraph, j) => (
+        <p key={j}>{paragraph}</p>
+      ))}
+    </div>
+  );
+
+  let buffs = [],
+    debuffs = [];
+
+  character.buffs.forEach((currentBuff) => {
+    if (currentBuff.isDebuff) debuffs.push(currentBuff);
+    else buffs.push(currentBuff);
+  });
+
+  buffs = buffs.map(parseBuff);
+  debuffs = debuffs.map(parseBuff);
+
+  return (
+    <div className="grid-container row-flex">
+      <div className="col-1_2 grid-tile">
+        {buffs.length > 0 ? buffs : <p>-None-</p>}
+      </div>
+      <div className="col-1_2 grid-tile">
+        {debuffs.length > 0 ? debuffs : <p>-None-</p>}
+      </div>
+    </div>
+  );
+};
+
+export default CharacterBuffsTab;
