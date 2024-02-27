41fbf470afb3f0a8a3aa3647d271baa5dc013b17
Tristan <127442375+tyoung-99@users.noreply.github.com>
2024-02-27 13:10:00 -0500

    Revert race handling to simpler version

--- backend/src/handleRaces.js
+++ backend/src/handleRaces.js
@@ -3,40 +3,27 @@ import { ObjectId } from "mongodb";
 export const getRaces = async (db) => {
   const collection = db.collection("races");
   const races = await collection.find({}).toArray();
-  console.log(races);
   return races;
 };
 
-export const getOneRace = async (db, raceId) => {
-  return getOneHelper(db, "races", raceId);
-};
-
-export const getSubraces = async (db, raceId) => {
+export const getSubracesFromParent = async (db, raceId) => {
   const collection = db.collection("subraces");
   const subraces = await collection.find({ parentRace: raceId }).toArray();
-  console.log(subraces);
   return subraces;
 };
 
-export const getOneSubrace = async (db, subraceId) => {
-  return getOneHelper(db, "subraces", subraceId);
+export const getOneRace = async (db, raceId) => {
+  const collection = db.collection("races");
+  const race = await collection.findOne({
+    _id: ObjectId.createFromHexString(raceId),
+  });
+  return race;
 };
 
-const getOneHelper = async (db, collectionName, id) => {
-  const mainCollection = db.collection(collectionName);
-  const featureCollection = db.collection("racialFeatures");
-
-  const race = await mainCollection.findOne({
-    _id: ObjectId.createFromHexString(id),
+export const getOneSubrace = async (db, subraceId) => {
+  const collection = db.collection("subraces");
+  const subrace = await collection.findOne({
+    _id: ObjectId.createFromHexString(subraceId),
   });
-
-  for (let i = 0; i < race.features.length; i++) {
-    race.features[i] = await featureCollection.findOne({
-      _id: ObjectId.createFromHexString(race.features[i]),
-    });
-  }
-
-  console.log(race);
-
-  return race;
+  return subrace;
 };
